b5d81c637601d4b4ef9e017d053b235c3a4aa4aa
Daniel <oopdaniel@gmail.com>
2019-06-02 22:30:41 -0700

    connect to AWS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const FIXED_ADMIN_ADDR = '0x8cc5dbc1bae182ca2aaa8d81420f5cf67765341a';
-export const FIXED_ADMIN_PASSWORD = '123456';
+// export const FIXED_ADMIN_ADDR = '0x8cc5dbc1bae182ca2aaa8d81420f5cf67765341a';
+// export const FIXED_ADMIN_PASSWORD = '123456';
+export const FIXED_ADMIN_ADDR = '0x276446d05fddc77aff785393ba04a188aa4c23e0';
+export const FIXED_ADMIN_PASSWORD = 'password';
 export const FIXED_INITIAL_AMOUNT = 80;
 export const FIXED_PASSWORD = 'password';
